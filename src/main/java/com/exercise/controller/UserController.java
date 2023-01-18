f0605e3b7b1e155ab87200f8df9d38c58305c202
Edisson Castro <edissoncastro.11@hotmail.com>
2023-01-17 20:34:41 -0500

    Implementacion para compartir instancias User desde la capa Service

--- src/main/java/com/exercise/controller/UserController.java
+++ src/main/java/com/exercise/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.exercise.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.exercise.service.UserService;
 
 @Controller
 public class UserController {
-	
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/")
+	public String getAllUsers(Model model) {
+		var users = userService.getAllUsers();
+		model.addAttribute("users", users);
+		return "index";
+	}
+
 }
--- src/main/java/com/exercise/service/impl/UserServiceImpl.java
+++ src/main/java/com/exercise/service/impl/UserServiceImpl.java
@@ -1,21 +1,47 @@
 package com.exercise.service.impl;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 import com.exercise.domain.User;
 import com.exercise.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<User> getAllUsers() {
-		var users = new ArrayList<User>();
+		var u1 = new User();
+		var u2 = new User();
+		var u3 = new User();
+		var u4 = new User();
+		var u5 = new User();		
 		
+		u1.setName("Edisson");
+		u1.setLastName("Castro");
+		u1.setEmail("edissoncastro.11@hotamil.com");
 		
+		u2.setName("Nubia");
+		u2.setLastName("Piza");
+		u2.setEmail("napiza@misena.edu.co");
 		
+		u3.setName("John");
+		u3.setLastName("Doe");
+		u3.setEmail("john.doe@mail.com");
 		
-		return null;
+		u4.setName("Beto");
+		u4.setLastName("Fya");
+		u4.setEmail("beto.fya@fakemail.com");
+		
+		u5.setName("Ricardo");
+		u5.setLastName("Bustos");
+		u5.setEmail("ricardo.bustos@arra-in.co");
+		
+		var users = Arrays.asList(u1, u2, u3, u4, u5);
+		
+		return users;	
 	}
 
 }
